b2ea3eac83da047c6113b4cab7f04e02a70286c6
Armen Arakelian <yozhyn1@gmail.com>
2017-10-30 00:27:30 +0200

    fixes

--- first contract/DepositHandling.sol
+++ first contract/DepositHandling.sol
@@ -4,33 +4,30 @@ contract DepositHandling {
     struct Deposit {
         uint balance;
         bool lended;
+        address host;
     }
    
-    address host;
     mapping(address => Deposit) public deposit;
     event Borrow(address receiver, uint amount);
     event Refund(address receiver, uint amount);
     
     
     function borrowMoney(uint amount, address _host) public {
-        if(amount == 0)
-            revert();
         if(deposit[msg.sender].lended == true)
             revert();
-        deposit[msg.sender].balance = 0; 
-        host = _host;
+        deposit[msg.sender].host = _host;
         deposit[msg.sender].balance += amount;
         if(amount > 0)
             deposit[msg.sender].lended = true;
         Borrow(msg.sender, amount);
     }
     
-    function RefundMoney(address receiver, uint amount)public {
+    function RefundMoney(uint amount, address receiver)public {
         if (deposit[receiver].lended == false)
             revert();
         if(amount > deposit[receiver].balance)
             revert();
-        if(msg.sender != host)
+        if(msg.sender != deposit[receiver].host)
             revert();
         deposit[receiver].balance -= amount;
         if(deposit[receiver].balance == 0)
